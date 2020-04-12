8110a8207641c8af3bfa90c4fbe697241bc3e17d
Ave Nurme <nurme.ave@gmail.com>
2020-04-12 17:23:06 +0300

    Update hangman.py
    Added one more animal to the list WORDS_TO_GUESS and also improved the visual side to the user in the console.

--- hangman.py
+++ hangman.py
@@ -1,16 +1,17 @@
 """Modelling a classic Hangman game in English (without hanging though...)."""
 
-import string
-import random
+from string import ascii_lowercase
+from random import choice
 
-ALPHABET_LIST = list(string.ascii_lowercase)
+ALPHABET_LIST = list(ascii_lowercase)
 WORDS_TO_GUESS = ["fox",
-                  "bear",
+                  "wolf",
                   "koala",
                   "hedgehog",
                   "chimpanzee",
-                  "chinchilla"]
-RANDOM_WORD = random.choice(WORDS_TO_GUESS)
+                  "chinchilla",
+                  "rhinoceros"]
+RANDOM_WORD = choice(WORDS_TO_GUESS)
 LETTERS_IN_WORD, LETTERS_NOT_IN_WORD = [], []
 BASE = ['_' for letter in list(RANDOM_WORD)]
 
@@ -19,7 +20,7 @@ def guess_the_word():
     """Perform some action based on user input."""
 
     print(f"\nHere's the word I need you to guess: "
-          f"{len(RANDOM_WORD) * ' _'}\n")
+          f"{len(RANDOM_WORD) * ' _'}")
 
     while sorted(LETTERS_IN_WORD) != sorted(RANDOM_WORD):
         user_input = input("\nPlease give a single letter: ").lower()
@@ -33,11 +34,12 @@ def guess_the_word():
                     LETTERS_IN_WORD.append(user_input)
             else:
                 LETTERS_NOT_IN_WORD.append(user_input)
-                print(f"Sorry - please try again!"
-                      " Here's the list of letters you've"
+                print(f">>>> Sorry - please try again! "
+                      f"'{user_input}' does not exist in this word..."
+                      " \n\t Here's the list of letters you've"
                       " tried which do not exist in the word:"
-                      f"\n{LETTERS_NOT_IN_WORD}")
-            print("".join(BASE).capitalize())
+                      f"\n\t {LETTERS_NOT_IN_WORD}")
+            print(">>>> " + "".join(BASE).capitalize())
         else:
             print(f"'{user_input}' is not a valid value...")
             continue
